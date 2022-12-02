eb6e07c03c920db3d0a3410f8531a703d26f56cd
p-bzh <ronan.questier@mailfence.com>
2022-12-02 23:39:43 +0200

    day 2 part 2

--- day_2/script.py
+++ day_2/script.py
@@ -1,27 +1,52 @@
 player_score = 0
 
 with open("input.txt", "r") as turns:
-  for turn in turns:
-    opponent_choice = turn[0]
-    player_choice = turn[2]
-    if player_choice == "X" and opponent_choice == "A":
-      player_score += 3
-    elif player_choice == "Y" and opponent_choice == "B":
-      player_score += 3
-    elif player_choice == "Z" and opponent_choice == "C":
-      player_score += 3
-    else:
-      if player_choice == "X" and opponent_choice == "C":
-        player_score += 6
-      if player_choice == "Z" and opponent_choice == "B":
-        player_score += 6
-      if player_choice == "Y" and opponent_choice == "A":
-        player_score += 6
-    if player_choice == "X":
-      player_score += 1
-    if player_choice == "Y":
-      player_score += 2
-    if player_choice == "Z":
-      player_score += 3
+
+    for turn in turns:
+        opponent_choice = turn[0]
+        end = turn[2]
+        player_choice = ""
+        if end == "Y":
+            if opponent_choice == "A":
+                player_choice = "X"
+            elif opponent_choice == "B":
+                player_choice = "Y"
+            elif opponent_choice == "C":
+                player_choice = "Z"
+        elif end == "X":
+            if opponent_choice == "A":
+                player_choice = "Z"
+            elif opponent_choice == "C":
+                player_choice = "Y"
+            elif opponent_choice == "B":
+                player_choice = "X"
+        else:
+            if opponent_choice == "A":
+                player_choice = "Y"
+            elif opponent_choice == "C":
+                player_choice = "X"
+            elif opponent_choice == "B":
+                player_choice = "Z"
+
+        print(player_choice)
+        if player_choice == "X" and opponent_choice == "A":
+            player_score += 3
+        elif player_choice == "Y" and opponent_choice == "B":
+            player_score += 3
+        elif player_choice == "Z" and opponent_choice == "C":
+            player_score += 3
+        else:
+            if player_choice == "X" and opponent_choice == "C":
+                player_score += 6
+            if player_choice == "Z" and opponent_choice == "B":
+                player_score += 6
+            if player_choice == "Y" and opponent_choice == "A":
+                player_score += 6
+        if player_choice == "X":
+            player_score += 1
+        if player_choice == "Y":
+            player_score += 2
+        if player_choice == "Z":
+            player_score += 3
 
 print(player_score)
